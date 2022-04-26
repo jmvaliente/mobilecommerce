80a51268d8c4b68bd708680cbb9dd56a8b41b474
jmvaliente <valiente_jose2@hotmail.com>
2022-04-27 00:52:46 +0200

    feat(search component): added search component UI

---
+++ src/ui/components/search/Search.jsx
@@ -0,0 +1,22 @@
+const Search = () => {
+  return (
+    <div class='level-right'>
+      <div class='level-item'>
+        <div class='field has-addons'>
+          <p class='control'>
+            <input class='input' type='text' placeholder='Find a post' />
+          </p>
+          <p class='control'>
+            <button class='button'>
+              Search
+            </button>
+          </p>
+        </div>
+      </div>
+      <div class='level-item' />
+      <div class='level-item' />
+    </div>
+  )
+}
+
+export default Search
