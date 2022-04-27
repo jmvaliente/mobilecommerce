4323b5160bf50120efc9997e4556dd348882c533
jmvaliente <valiente_jose2@hotmail.com>
2022-04-27 13:01:25 +0200

    feat(page detail): created logic fetch data

--- src/ui/pages/ProductDetail/enhancer.js
+++ src/ui/pages/ProductDetail/enhancer.js
@@ -1,6 +1,19 @@
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { getMobileDetail } from '../../../services/service'
+
 const enhance = View => {
   const ProductDetail = () => {
-    return <View />
+    const { id } = useParams()
+    const [mobileDetail, setMobileDetail] = useState({})
+    useEffect(() => {
+      const fetchMobileDetailData = async () => {
+        const data = await getMobileDetail(id)
+        setMobileDetail(data)
+      }
+      fetchMobileDetailData()
+    }, [])
+    return <View mobileDetail={mobileDetail} />
   }
   return ProductDetail
 }
