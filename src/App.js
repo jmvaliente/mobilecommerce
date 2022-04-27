16a1f0f2ae9fa5486d445b83705ad6224e2ea85a
jmvaliente <valiente_jose2@hotmail.com>
2022-04-27 09:25:44 +0200

    build(esLint): added esLint config Standard in proyect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './ui/pages/Home'
-import './App.css';
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
+    <div className='App'>
       <div className='App'>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/:id' element={<h1>Page Detail</h1>} />
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/:id' element={<h1>Page Detail</h1>} />
+        </Routes>
+      </div>
     </div>
-    </div>
-  );
+  )
 }
 
-export default App;
+export default App
