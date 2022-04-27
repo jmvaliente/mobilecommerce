a3ed48a4954ee38e24da0cd0073add4f37e3883a
jmvaliente <valiente_jose2@hotmail.com>
2022-04-27 13:03:08 +0200

    feat(page detail): updated image detail

--- src/ui/pages/ProductDetail/View.jsx
+++ src/ui/pages/ProductDetail/View.jsx
@@ -3,20 +3,21 @@ import Image from '../../components/image/Image'
 import Actions from '../../components/actions/Actions'
 import Description from '../../components/description/Description'
 
-const ProductDetail = () => {
+const ProductDetail = ({ mobileDetail }) => {
+  const { imgUrl } = mobileDetail
   return (
     <div className='container is-widescreen'>
-      <div class='tile is-ancestor'>
-        <div class='tile is-6 is-parent'>
-          <div class='tile is-child box'>
-            <Image />
+      <div className='tile is-ancestor'>
+        <div className='tile is-6 is-parent'>
+          <div className='tile is-child box'>
+            <Image imgUrl={imgUrl} />
           </div>
         </div>
-        <div class='tile is-vertical is-parent'>
-          <div class='tile is-child box'>
+        <div className='tile is-vertical is-parent'>
+          <div className='tile is-child box'>
             <Description />
           </div>
-          <div class='tile is-child box'>
+          <div className='tile is-child box'>
             <Actions />
           </div>
         </div>
