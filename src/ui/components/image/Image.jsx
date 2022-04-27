f3528cd99ee68adb0c075a24f9b25553769d58e4
jmvaliente <valiente_jose2@hotmail.com>
2022-04-27 12:45:16 +0200

    feat(page detail): added components in details page

--- src/ui/components/image/Image.jsx
+++ src/ui/components/image/Image.jsx
@@ -1,6 +1,8 @@
 const Image = () => {
   return (
-    <h1>Image</h1>
+    <figure class='image is-square'>
+      <img src='https://bulma.io/images/placeholders/256x256.png' />
+    </figure>
   )
 }
 
