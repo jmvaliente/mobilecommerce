6a26216b7fed11309631b81bce3a0a620b420f0b
jmvaliente <valiente_jose2@hotmail.com>
2022-04-27 11:41:55 +0200

    feat(enhancer Home): created logic for view enhance

--- src/ui/pages/Home/enhancer.js
+++ src/ui/pages/Home/enhancer.js
@@ -1,6 +1,20 @@
+import { useState, useEffect } from 'react'
+import { getMobileList } from '../../../services/service'
 const enhance = View => {
   const Home = () => {
-    return <View />
+    const [mobileList, setMobileList] = useState([])
+    useEffect(() => {
+      const fetchMobileData = async () => {
+        const data = await getMobileList()
+        const listGropup = data.map((mobile, idx) => {
+          return idx % 4 === 0 ? data.slice(idx, idx + 4) : null
+        }).filter(gropup => gropup)
+        setMobileList(listGropup)
+      }
+      fetchMobileData()
+    }, [])
+
+    return <View mobileList={mobileList} />
   }
   return Home
 }
