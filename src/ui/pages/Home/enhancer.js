1bfba5b8cd2669b6a85cf6eebf49daff7060abad
jmvaliente <valiente_jose2@hotmail.com>
2022-04-27 17:27:47 +0200

    feat(search bar): create logic to search devices

--- src/ui/pages/Home/enhancer.js
+++ src/ui/pages/Home/enhancer.js
@@ -1,19 +1,23 @@
 import { useState, useEffect } from 'react'
 import { getMobileList } from '../../../services/service'
 import { listGroup } from '../../utils/listGroup'
+import { mobileFilter } from '../../utils/search'
 const enhance = View => {
   const Home = () => {
     const [mobileList, setMobileList] = useState([])
+    const [search, setSearch] = useState('')
+
     useEffect(() => {
       const fetchMobileData = async () => {
         const data = await getMobileList()
-        const listGropup = listGroup(data, 4)
+        const mobileDataFilter = mobileFilter(data, search)
+        const listGropup = listGroup(mobileDataFilter, 4)
         setMobileList(listGropup)
       }
       fetchMobileData()
-    }, [])
+    }, [search])
 
-    return <View mobileList={mobileList} />
+    return <View mobileList={mobileList} setSearch={setSearch} />
   }
   return Home
 }
