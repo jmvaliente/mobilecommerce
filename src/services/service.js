ea886f9e864129ffa36a74a826b34ed35abc22f4
jmvaliente <valiente_jose2@hotmail.com>
2022-04-27 09:26:47 +0200

    fix(services): fixed error esLint in service file

--- src/services/service.js
+++ src/services/service.js
@@ -1,6 +1,7 @@
 const URL_API = 'https://front-test-api.herokuapp.com'
 
 export const getMobileList = async () => {
+  // eslint-disable-next-line
   const data = await fetch(`${URL_API}/api/product`)
     .then(response => response.json())
     .then(data => data)
@@ -8,6 +9,7 @@ export const getMobileList = async () => {
 }
 
 export const getMobileDetail = async (id) => {
+  // eslint-disable-next-line
   const data = await fetch(`${URL_API}/api/product/${id}`)
     .then(response => response.json())
     .then(data => data)
@@ -15,6 +17,7 @@ export const getMobileDetail = async (id) => {
 }
 
 export const addCartMobile = async (dataMobile) => {
+  // eslint-disable-next-line
   const data = await fetch(`${URL_API}/api/cart`, {
     method: 'POST',
     body: dataMobile
