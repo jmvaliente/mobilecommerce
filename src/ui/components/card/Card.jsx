218c5dafd31a2fbe18c6fb4c06bbf733bbebc653
jmvaliente <valiente_jose2@hotmail.com>
2022-04-28 09:53:58 +0200

    fix(card): fixed card size

--- src/ui/components/card/Card.jsx
+++ src/ui/components/card/Card.jsx
@@ -1,24 +1,26 @@
 const Card = ({ mobile }) => {
   const { imgUrl, model, price, brand } = mobile
   return (
-    <div className='container'>
-      <div className='card'>
-        <div className='card-image'>
-          <figure className='image is-4by3'>
-            <img src={imgUrl} alt={model} />
+    <div className='section'>
+      <section className='columns is-centered'>
+        <div className='card'>
+          <figure className='image'>
+            <img className='image is-128x128' src={imgUrl} alt={model} />
+
           </figure>
-        </div>
-        <div className='card-content'>
-          <div className='media'>
-            <div className='media-left' />
-            <div className='media-content'>
-              <p className='title is-4'>{brand}</p>
-              <p className='subtitle is-10'>{model}</p>
-              <p className='subtitle is-6'>{price || 'Sin Stock'}</p>
+
+          <div className='card-content'>
+            <div className='media'>
+              <div className='media-left' />
+              <div className='media-content'>
+                <p className='title is-4'>{brand}</p>
+                <p className='subtitle is-10'>Model: {model}</p>
+                <p className='subtitle is-6'>Price: {price || 'Price on request'}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   )
 }
